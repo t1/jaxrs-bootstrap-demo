f4348ca85151d4d275542cc41a6774aeabddc096
Ruediger zu Dohna <snackbox@sinntr.eu>
2024-06-16 06:49:30 +0200

    minor simplification

--- src/test/java/test/integration/HelloIT.java
+++ src/test/java/test/integration/HelloIT.java
@@ -4,6 +4,7 @@ import com.example.HelloResource;
 import jakarta.ws.rs.SeBootstrap;
 import jakarta.ws.rs.client.Client;
 import jakarta.ws.rs.client.ClientBuilder;
+import jakarta.ws.rs.client.WebTarget;
 import jakarta.ws.rs.core.Application;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
@@ -19,27 +20,29 @@ public class HelloIT {
     @BeforeAll
     static void bootstrap() {
         SeBootstrap
-            // to use the actual HelloApplication class, we'd have to explicitly list the classes there...
-            // auto discovery works in the container, but not in SeBootstrap
-            .start(new Application() {
-                @Override public Set<Class<?>> getClasses() {
-                    return Set.of(HelloResource.class);
-                }
-            })
-            .thenApply(instance -> {
-                BASE_URI = instance.configuration().baseUri();
-                System.out.println("Bootstrapped on " + BASE_URI);
-                return instance;
-            })
-            .toCompletableFuture().join();
+                // to use the actual HelloApplication class, we'd have to explicitly list the classes there...
+                // auto discovery works in the container, but not in SeBootstrap
+                .start(new Application() {
+                    @Override public Set<Class<?>> getClasses() {
+                        return Set.of(HelloResource.class);
+                    }
+                })
+                .thenApply(instance -> {
+                    BASE_URI = instance.configuration().baseUri();
+                    System.out.println("Bootstrapped on " + BASE_URI);
+                    return instance;
+                })
+                .toCompletableFuture().join();
     }
 
     static URI BASE_URI;
     private static final Client CLIENT = ClientBuilder.newClient();
 
+    private final WebTarget service = CLIENT.target(BASE_URI);
+
     @Test
-    void shouldGetGreeting() {
-        var response = CLIENT.target(BASE_URI).path("/hello").request().get();
+    void shouldGetGreetingResponse() {
+        var response = service.path("/hello").request().get();
 
         then(response.getStatusInfo()).isEqualTo(OK);
         then(response.readEntity(String.class)).isEqualTo("Hello from Service");
